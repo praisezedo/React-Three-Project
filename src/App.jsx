712d257417290653cbba46d9a864f9ac552f396a
Praise Innocent <praiseinnocent049@gmail.com>
2025-10-28 06:37:51 +0100

    Added Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Hero from "./Hero.jsx";
 import NavBar from "./NavBar.jsx";
 export default function App() {
    return (
     <>
   <NavBar/>
+  <Hero/>
     </>
    );
 }
---
+++ src/Hero.jsx
@@ -0,0 +1,24 @@
+import MackBookTitle from './public/title.png'
+import HeroVideo from './public/videos/hero.mp4'
+import { useRef , useEffect } from 'react'
+function Hero() {
+    const videoRef = useRef();
+
+    useEffect(() => {
+        if(videoRef.current) {videoRef.current.playbackRate = 2};
+    },[]);
+return (
+<>
+<section id="hero">
+    <div>
+        <h1>MacBook Pro</h1>
+        <img src={MackBookTitle} alt='Mackbooktitle'/>
+    </div>
+    <video ref={videoRef} src={HeroVideo} autoPlay muted playsInline/>
+    <button>Buy</button>
+    <p>From $1599 to $133/month for 12 months</p>
+</section>
+</>
+);
+}
+export default Hero
